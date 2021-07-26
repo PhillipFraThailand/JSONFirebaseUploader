983e5767fdee6507406610f1c13161893b02480c
philiong <phillip_eismark@hotmail.dk>
2021-07-26 18:24:36 +0200

    it forggen works maite!!!

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const admin = require('./node_modules/firebase-admin');
 const serviceAccount = require("./serviceAccountKey.json");
+
 const data = require("./data.json");
 const collectionKey = "exercises"; //collection name
 
@@ -13,11 +14,14 @@ const settings = { timestampsInSnapshots: true };
 firestore.settings(settings);
 
 if (data && (typeof data === "object")) {
+  // console.log(data)
   Object.keys(data).forEach(docKey => {
-    // firestore.collection(collectionKey).doc(docKey).set(data[docKey]).then((res) => { //uncomment to set docId yourself
-    firestore.collection(collectionKey)
-      .doc(docKey)
-      .set(data[{ docKey }])
+    console.log(data[docKey])
+    // // firestore.collection(collectionKey).doc(docKey).set(data[docKey]).then((res) => { //uncomment to set docId yourself
+    firestore
+      .collection(collectionKey)
+      .doc()
+      .set(data[docKey])
       .then((res) => {
         console.log("Document " + docKey + " successfully written!");
       }).catch((error) => {
